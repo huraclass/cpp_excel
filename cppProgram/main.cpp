d3181969b8c0298315e0c1971accd912b8bad954
huraclass <85816950+huraclass@users.noreply.github.com>
2023-03-07 20:31:42 +0900

    Initial Commit

---
+++ cppProgram/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cppProgram
+//
+//  Created by jinwoo Jung on 2023/03/07.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
